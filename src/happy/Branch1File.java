880ee3465153d65313bf849c17cc9ea1fbd5e33d
dhirsingh1985 <59521067+dhirsingh1985@users.noreply.github.com>
2021-04-04 14:48:36 +1000

    Update Branch1File.java

--- src/happy/Branch1File.java
+++ src/happy/Branch1File.java
@@ -8,8 +8,12 @@ public class Branch1File {
 
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>  Local new change  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>  New Change <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>  New Change 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>  New Change 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>  New Change 3 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		
 	}
 	
 	
