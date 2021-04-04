085da0379647750a4d9e2af65d2d7dc499f090a2
dhirsingh1985 <59521067+dhirsingh1985@users.noreply.github.com>
2021-04-04 10:59:21 +1000

    first change

---
+++ src/happy/location.java
@@ -0,0 +1,13 @@
+package happy;
+
+public class location {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>  MASTER <<<<<<<<<<<<<<<<<<<<<<<<<<<"); 
+		
+		
+	}
+	
+}
